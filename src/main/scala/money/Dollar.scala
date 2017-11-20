bdd563d5b21593b0aba2d6a7d46419076564e43a
Pommel Kurama <pommel.pg@gmail.com>
2017-11-20 22:30:28 +0900

    [add]add_dollar_class

---
+++ src/main/scala/money/Dollar.scala
@@ -0,0 +1,6 @@
+package com.github.kuramapommel.ScalaTDD.money
+
+case class Dollar( amount: Integer ) {
+
+  def times( multiplier: Integer ) : Unit = {}
+}
--- src/test/scala/money/MoneyTest.scala
+++ src/test/scala/money/MoneyTest.scala
@@ -1,11 +1,13 @@
-package money
+package com.github.kuramapommel.ScalaTDD.test.money
 
 import org.scalatest._
 
 class MoneyTest extends FlatSpec with Matchers {
 
-  "Multiplication" should {
-    val five = Doller( 5 )
+  import com.github.kuramapommel.ScalaTDD.money._
+
+  "Dollar" should "multiplication" in {
+    val five = Dollar( 5 )
     five.times( 2 )
     five.amount should be( 10 )
   }
