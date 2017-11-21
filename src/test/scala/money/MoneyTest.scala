b86eb791d7c76e7881e932ba5c6586eb4295ad08
Pommel Kurama <pommel.pg@gmail.com>
2017-11-22 00:15:06 +0900

    [add]use function object at money

--- src/test/scala/money/MoneyTest.scala
+++ src/test/scala/money/MoneyTest.scala
@@ -7,24 +7,31 @@ class MoneyTest extends FlatSpec with Matchers {
   import com.github.kuramapommel.scalatdd.money._
 
   "Dollar" should "multiplication" in {
-    val five = Money.dollar( 5 )
-    assert( Money.dollar( 10 ) == five.times( 2 ) )
-    assert( Money.dollar( 15 ) == five.times( 3 ) )
+    val money = Money.dollar
+    val five = money( 5 )
+
+    assert( money( 10 ) == five.times( 2 ) )
+    assert( money( 15 ) == five.times( 3 ) )
   }
 
   "Money" should "equality" in {
-    assert( Money.dollar( 5 ) == Money.dollar( 5 ) )
-    assert( Money.dollar( 5 ) != Money.dollar( 6 ) )
+    val dollar = Money.dollar
+    val franc = Money.franc
 
-    assert( Money.franc( 5 ) == Money.franc( 5 ) )
-    assert( Money.franc( 5 ) != Money.franc( 6 ) )
+    assert( dollar( 5 ) == dollar( 5 ) )
+    assert( dollar( 5 ) != dollar( 6 ) )
 
-    assert( Money.franc( 5 ) != Money.dollar( 5 ) )
+    assert( franc( 5 ) == franc( 5 ) )
+    assert( franc( 5 ) != franc( 6 ) )
+
+    assert( franc( 5 ) != dollar( 5 ) )
   }
 
   "Franc" should "multiplication" in {
-    val five = Money.franc( 5 )
-    assert( Money.franc( 10 ) == five.times( 2 ) )
-    assert( Money.franc( 15 ) == five.times( 3 ) )
+    val money = Money.franc
+    val five = money( 5 )
+
+    assert( money( 10 ) == five.times( 2 ) )
+    assert( money( 15 ) == five.times( 3 ) )
   }
 }
