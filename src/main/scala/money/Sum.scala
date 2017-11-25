cf85f3e27ef7ae5b63f2f1e435556cdc1fc0e015
Pommel Kurama <pommel.pg@gmail.com>
2017-11-25 14:33:02 +0900

    [add]add sum times test

--- src/main/scala/money/Sum.scala
+++ src/main/scala/money/Sum.scala
@@ -2,7 +2,7 @@ package com.github.kuramapommel.scalatdd.money
 
 final case class Sum( augend: Expression, addend: Expression ) extends Expression {
 
-  override def +( addend: Expression ) = null
+  override def +( addend: Expression ) = Sum( this, addend )
 
   override def reduce( bank: Bank, to: String ) = {
     val amount = augend.reduce( bank, to ).amount + addend.reduce( bank, to ).amount
--- src/test/scala/money/MoneyTest.scala
+++ src/test/scala/money/MoneyTest.scala
@@ -93,6 +93,17 @@ class MoneyTest extends FlatSpec with Matchers {
     val result = bank.reduce( sum, "USD" )
 
     assert( result == Money.dollar( 15 ) )
+  }
+
+  "Sum" should "have times" in {
+    val fiveBucks = Money.dollar( 5 )
+    val tenFrancs = Money.franc( 10 )
+    val bank = new Bank
+    bank.addRate( "CHF", "USD", 2 )
+    val sum = Sum( fiveBucks, tenFrancs ).times( 2 )
+    val result = bank.reduce( sum, "USD" )
+
+    assert( result == Money.dollar( 20 ) )
 
   }
 }
